99315724af3ca08170bfdc96be2489339d1dea73
veladan <dani_vela@me.com>
2012-07-13 14:27:20 +0200

    added image downloader

---
+++ OSLibrary/UIImageView+Downloader.h
@@ -0,0 +1,15 @@
+//
+//  UIView+Downloader.h
+//  netapp
+//
+//  Created by Daniel Vela on 13/07/12.
+//  Copyright (c) 2012 Inycom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImageView (Downloader)
+
+-(void)setImageFrom:(NSString*)url withTag:(NSInteger)tag;
+
+@end
