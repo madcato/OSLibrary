51dcdb64b78ef17ecc2eb559280b33dcdc754465
Daniel Vela <dani_vela@me.com>
2016-01-21 19:30:11 +0100

    Added email validator

---
+++ OSLibrary/NSEmailValidator.h
@@ -0,0 +1,15 @@
+//
+//  NSEmailValidator.h
+//  OSLibrary
+//
+//  Created by Daniel Vela on 21/01/16.
+//  Copyright © 2016 veladan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSEmailValidator : NSObject
+
+- (BOOL)isValid:(NSString*)email;
+
+@end
