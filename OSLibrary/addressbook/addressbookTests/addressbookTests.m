a79eb583a9ccaddfb9ba6a14432e6784488de68d
Daniel Vela <dani_vela@me.com>
2016-04-13 16:19:20 +0200

    Removed unit tests scripts

--- OSLibrary/addressbook/addressbookTests/addressbookTests.m
+++ OSLibrary/addressbook/addressbookTests/addressbookTests.m
@@ -8,13 +8,19 @@
 
 #import "addressbookTests.h"
 
+@interface addressbookTests ()
+
+@property (nonatomic, strong) NSManagedObjectContext* managedObjectContext;
+
+@end
+
 @implementation addressbookTests
 
 - (void)setUp
 {
     [super setUp];
-    
-    // Set-up code here.
+
+    self.managedObjectContext = [[OSDatabase defaultDatabase] managedObjectContext];
 }
 
 - (void)tearDown
@@ -24,9 +30,46 @@
     [super tearDown];
 }
 
-- (void)testExample
+- (void)testCreateSomeObjects
 {
-    XCTFail(@"Unit tests are not implemented yet in addressbookTests");
+    [self insertNewObject:nil];
+    [self insertNewObject:nil];
+    [self insertNewObject:nil];
+    [[OSCoreDataSyncEngine sharedEngine] startSync];
+    
+    
+}
+
+- (NSManagedObject*)insertNewObject:(id)sender
+{
+    NSManagedObjectContext *context = self.managedObjectContext;
+    NSManagedObject *newManagedObject = [NSEntityDescription insertNewObjectForEntityForName:@"Person" inManagedObjectContext:context];
+    
+    // If appropriate, configure the new managed object.
+    // Normally you should use accessor methods, but using KVC here avoids the need to add a custom class to the template.
+    [newManagedObject setValue:[NSDate date] forKey:@"birthDate"];
+    [newManagedObject setValue:@1.80f forKey:@"height"];
+    [newManagedObject setValue:@"Dani Vela" forKey:@"name"];
+    [newManagedObject setValue:@YES forKey:@"programmer"];
+    [newManagedObject setValue:[NSData data] forKey:@"avatar"];
+    [newManagedObject setValue:[NSNumber numberWithInt:OSObjectCreated] forKey:@"syncStatus"];
+    // Save the context.
+    NSError *error = nil;
+    if (![context save:&error]) {
+        // Replace this implementation with code to handle the error appropriately.
+        // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+        NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+        abort();
+    }
+    return newManagedObject;
+}
+
+- (void)updateObject:(NSManagedObject*)object {
+    
+    if (object == nil) return;
+    
+    [object setValue:@"Star Wars" forKey:@"name"];
+    [OSCoreDataSyncEngine updateObjectAndSave:object inContext:self.managedObjectContext];
 }
 
 @end
